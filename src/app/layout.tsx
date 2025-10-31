a8576a5b321174f1ef168024c80b266be0c6c890
dev02-hue <trustwallet2758604@gmail.com>
2025-10-31 11:21:39 +0100

    smart

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
  
 import LayoutWrapper from "./component/layout/LayoutWrapper";
-  import TawkToScript from "./component/layout/TawkToScript";
+ 
 import SmartsuppScript from "./component/layout/TawkToScript";
 
 const geistSans = Geist({
